f348493e31cad509a61dfa5c8ac9f00e1970cb30
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2025-05-26 23:11:35 +0530

    adsa

---
+++ build/precache-manifest.a4731733825a1163aefaa12b63833f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb3fd44fd647c358876409de3dfbc13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0a52485cbff84fe35bb",
+    "url": "/static/css/main.ba0e3544.chunk.css"
+  },
+  {
+    "revision": "b4d519b9880aa253b5d7",
+    "url": "/static/js/2.a27e2320.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.a27e2320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a52485cbff84fe35bb",
+    "url": "/static/js/main.97126fd3.chunk.js"
+  },
+  {
+    "revision": "430e6bf337df4dfe427b",
+    "url": "/static/js/runtime-main.8dbc5147.js"
+  }
+]);
